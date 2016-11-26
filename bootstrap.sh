641cc0d3da71f745a7bb5a26503340ca1a22bce0
Magnus Eriksson <magnus.eriksson@apegroup.com>
2016-11-26 11:19:52 +0100

    Additions
    - Remove ask-become-pass
    - Fetch and update ansible
    - Add casks iterm2, p4merge and slack

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+#Install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" ; \
+brew update ; \
+
+#Install ansible
+brew install ansible ; \
+
 #Run ansible using the inventory file located at 'ansible/inventory'
-ansible-playbook --ask-become-pass -i ansible/inventory ansible/setup-dotfiles.yaml --extra-vars "dotfilespath=`pwd`"
+ansible-playbook -i ansible/inventory ansible/setup-dotfiles.yaml --extra-vars "dotfilespath=`pwd`"
